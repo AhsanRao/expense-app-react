e4ecd3d3d3616295f713aee0cb347d4af06306a3
AhsanRao <raoahsan110@gmail.com>
2024-05-11 02:17:54 +0500

    Initial commit

---
+++ src/_mock/account.js
@@ -0,0 +1,11 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Expense Admin',
+  username: 'admin',
+  email: 'ash@g.cc',
+  password: 'open',
+  photoURL: '/assets/images/avatars/avatar_25.jpg',
+};
+
+export default account;
