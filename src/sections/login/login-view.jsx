74f9ce2adacbe108cc1069a24162521e724521d4
AhsanRao <raoahsan110@gmail.com>
2024-05-11 02:23:38 +0500

    Update login-view.jsx

--- src/sections/login/login-view.jsx
+++ src/sections/login/login-view.jsx
@@ -15,7 +15,7 @@ import InputAdornment from '@mui/material/InputAdornment';
 import { useRouter } from 'src/routes/hooks';
 
 import { bgGradient } from 'src/theme/css';
-import { account } from 'src/_mock/account';
+import account from 'src/_mock/account';
 import AuthContext from 'src/context/AuthContext';
 
 import Logo from 'src/components/logo';
